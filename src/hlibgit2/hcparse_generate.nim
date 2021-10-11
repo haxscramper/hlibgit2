950dad28f4476c9bca84a9c2f9ba1ea365dadf5b
haxscramper <haxscramper@gmail.com>
2021-10-11 20:43:03 +0300

    [CLEAN] Use compacted API

--- src/hlibgit2/hcparse_generate.nim
+++ src/hlibgit2/hcparse_generate.nim
@@ -1,38 +1,12 @@
 import hcparse
 
-import
-  hmisc/other/[oswrap, hshell],
-  hmisc/core/all
-
-startHax()
-
-
-let
-  outDir = oswrap.currentSourceDir()
-  tmpDir = getAppTempDir() / "v3"
-
-let lib = AbsDir"/usr/include/git2"
-var expandMap = expandViaWave(
-  listFiles(lib, ignoreNames = @["stdint"]),
-  tmpDir, baseCParseConf
+let resultGrouped = wrapCSharedLibViaTsWave(
+  inDir = AbsDir"/usr/include/git2",
+  outDir = currentAbsSourceDir(),
+  tmpDir = getAppTempDir() / "v4",
+  libName = "git2",
+  ignoreIn = @["stdint"]
 )
 
-rmFiles(outDir, @["nim"], @["hcparse_generate", "libgit2_config"])
-
-let
-  fixConf = initCSharedLibFixConf(
-    "git2", false, lib, expandMap)
-
-  resultWrapped = tmpDir.wrapViaTs(fixConf)
-  resultGrouped = writeFiles(outDir, resultWrapped, cCodegenConf)
-
-for file in resultGrouped:
-  echov file
-  try:
-    execShell shellCmd(nim, check, errormax = 3, $file)
-    echov "file ok"
-
-  except ShellError:
-    echov "file fail"
-
-echov "done"
+validateGenerated(resultGrouped)
+echo "done"
