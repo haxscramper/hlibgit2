5a0be958ff9f6aacd27e06ffebdb9af645d86295
haxscramper <haxscramper@gmail.com>
2021-11-14 15:15:17 +0300

    [TEST]

--- tests/test1.nim
+++ tests/test1.nim
@@ -1,7 +1,81 @@
+{.push warning[UnusedImport]:off.}
+
+{.passc: "-I/usr/include".}
+
 import hlibgit2/[
+  annotated_commit,
+  apply,
+  attr,
+  blame,
+  blob,
+  branch,
+  buffer,
+  cert,
+  checkout,
+  cherrypick,
+  clone,
+  commit,
+  common,
+  config,
+  credential_helpers,
+  credential,
+  cred_helpers,
+  deprecated,
+  describe,
+  diff,
+  errors,
+  filter,
   global,
+  graph,
+  ignore,
+  indexer,
+  index,
+  libgit2_config,
+  mailmap,
+  merge,
+  message,
+  net,
+  notes,
+  "object",
+  odb_backend,
+  odb,
+  oidarray,
+  oid,
+  pack,
+  patch,
+  pathspec,
+  proxy,
+  rebase,
+  refdb,
+  reflog,
+  refs,
+  refspec,
+  remote,
   repository,
-  errors
+  reset,
+  revert,
+  revparse,
+  revwalk,
+  signature,
+  stash,
+  status,
+  strarray,
+  submodule,
+  tag,
+  trace,
+  transaction,
+  transport,
+  tree,
+  types,
+  version,
+  worktree
 ]
 
 assert git_libgit2_init() == 1
+
+if false:
+  block:
+    var a: git_strarray
+
+  block:
+    var c: ptr git_credential
