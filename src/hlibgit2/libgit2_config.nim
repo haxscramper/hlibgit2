3756ac59743824e8ba2cf030b072eaea53b6e01f
haxscramper <haxscramper@gmail.com>
2021-11-16 21:42:11 +0300

    [REFACTOR] Use new-style binding macro

--- src/hlibgit2/libgit2_config.nim
+++ src/hlibgit2/libgit2_config.nim
@@ -1,16 +1,25 @@
-import std/[macros]
+const
+  libgit2LinkMode* {.strdefine.} = "dynlib"
+  libgit2Lib* {.strdefine.} = "libgit2.so"
 
-const libgit2Static* {.booldefine.}: bool = false
-const libgit2DynLib* {.strdefine.}: string = "libgit2.so"
+import std/[macros]
 
 macro git2Proc*(a: untyped): untyped =
   result = a
   result.addPragma(ident"importc")
-  when not libgit2Static:
+  when libgit2LinkMode == "dynlib":
     result.addPragma(nnkExprColonExpr.newTree(
-      ident"dynlib", newLit(libgit2DynLib)))
+      ident"dynlib", newLit(libgit2Lib)))
 
-    echo result.repr()
+  elif libgit2LinkMode in ["static", "dlink"]:
+    # Default dynamic or static linking, handled by user via `{.passl.}`
+    # etc.
+    discard
+
+  else:
+    {.error: "Invalid libgit2 link mode specified" &
+      " expected 'dynlib', 'static' or 'dlink', but got " &
+      libgit2LinkMode.}
 
 type
   git_iterator* {.importc, incompleteStruct.} = object
