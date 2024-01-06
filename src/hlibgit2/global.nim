4ba543e6f8ad0fb8cf6c3d6f8ed85d1c1bb035d2
haxscramper <haxscramper@gmail.com>
2024-01-06 22:00:33 +0400

    Generator v2

--- src/hlibgit2/global.nim
+++
@@ -1,4 +0,0 @@
-import "./libgit2_config.nim" ## From gen file
-proc git_libgit2_init*(): cint {.git2Proc, importc: "git_libgit2_init".}
-
-proc git_libgit2_shutdown*(): cint {.git2Proc, importc: "git_libgit2_shutdown".}
